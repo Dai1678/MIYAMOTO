bd1a1cce450d5c88ef2d9745aba2f454ba6b4358
Dai <dai7774@gmail.com>
2018-04-19 14:10:42 +0900

    create MainFragment

--- Android/app/src/main/java/com/example/justdoit/miyamoto/activity/MainActivity.kt
+++ Android/app/src/main/java/com/example/justdoit/miyamoto/activity/MainActivity.kt
@@ -3,11 +3,21 @@ package com.example.justdoit.miyamoto.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.justdoit.miyamoto.R
+import com.example.justdoit.miyamoto.fragment.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(savedInstanceState == null)  {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_fragment, MainFragment.getInstance())
+                    .addToBackStack(null)
+                    .commit()
+        }
+
     }
 }
---
+++ Android/app/src/main/java/com/example/justdoit/miyamoto/fragment/MainFragment.kt
@@ -0,0 +1,30 @@
+package com.example.justdoit.miyamoto.fragment
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.justdoit.miyamoto.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun getInstance() : MainFragment {
+            return MainFragment()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
