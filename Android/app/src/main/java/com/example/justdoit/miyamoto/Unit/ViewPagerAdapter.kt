78b6cf8ec585284768bbdbe37b1d66bdc08f2012
Dai <dai7774@gmail.com>
2018-04-21 22:52:26 +0900

    create viewPager

---
+++ Android/app/src/main/java/com/example/justdoit/miyamoto/Unit/ViewPagerAdapter.kt
@@ -0,0 +1,16 @@
+package com.example.justdoit.miyamoto.Unit
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+class ViewPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getItem(position: Int): Fragment {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ Android/app/src/main/java/com/example/justdoit/miyamoto/activity/TabActivity.kt
@@ -0,0 +1,16 @@
+package com.example.justdoit.miyamoto.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.justdoit.miyamoto.R
+
+class TabActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tab)
+
+
+
+    }
+}
