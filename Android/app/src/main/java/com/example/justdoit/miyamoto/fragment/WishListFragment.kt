fcdda5b8f36afb566f1af0d2d7c8f221a3cc091a
Dai <dai7774@gmail.com>
2018-04-21 19:01:02 +0900

    create WishListFragment

--- Android/app/src/main/java/com/example/justdoit/miyamoto/fragment/WishListFragment.kt
+++ Android/app/src/main/java/com/example/justdoit/miyamoto/fragment/WishListFragment.kt
@@ -1,7 +1,34 @@
 package com.example.justdoit.miyamoto.fragment
 
-class WishListFragment {
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.justdoit.miyamoto.R
+import kotlinx.android.synthetic.main.fragment_login_form.*
+import kotlinx.android.synthetic.main.fragment_wish_list.*
 
 
+class WishListFragment : Fragment() {
+
+    companion object {
+        fun getInstance() : WishListFragment {
+            return WishListFragment()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_wish_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        createWish.setOnClickListener { view ->
+
+        }
+    }
 
 }
