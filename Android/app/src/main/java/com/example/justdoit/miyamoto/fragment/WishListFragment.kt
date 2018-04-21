f61844bc4e8696482864710ba104aadd30d63dee
TaigaNatto <taigan3253@gmail.com>
2018-04-21 20:06:36 +0900

    やばみ

--- Android/app/src/main/java/com/example/justdoit/miyamoto/fragment/WishListFragment.kt
+++ Android/app/src/main/java/com/example/justdoit/miyamoto/fragment/WishListFragment.kt
@@ -5,12 +5,18 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+import com.example.justdoit.miyamoto.Pasilist.PasilistAdapter
+import com.example.justdoit.miyamoto.Pasilist.PasilistModel
 import com.example.justdoit.miyamoto.R
 import kotlinx.android.synthetic.main.fragment_login_form.*
 import kotlinx.android.synthetic.main.fragment_wish_list.*
 
 
 class WishListFragment : Fragment() {
+
+    var mWishlistAdapter: PasilistAdapter?=null
+    var mWishlist: ListView?=null
 
     companion object {
         fun getInstance() : WishListFragment {
@@ -29,6 +35,15 @@ class WishListFragment : Fragment() {
         createWish.setOnClickListener { view ->
 
         }
+
+        mWishlist=view.findViewById(R.id.list_wish)
+        mWishlistAdapter= PasilistAdapter(context!!,R.layout.item_pasilist)
+
+        for(i in 0..20){
+            val sample= PasilistModel(0,"東京駅","午後2時まで",3000)
+            mWishlistAdapter?.add(sample)
+        }
+        mWishlist?.adapter=mWishlistAdapter
     }
 
 }
